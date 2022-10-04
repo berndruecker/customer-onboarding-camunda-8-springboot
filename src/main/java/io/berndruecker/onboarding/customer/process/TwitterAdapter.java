e45d04cb65aebbd1990aa96fcba41e9892d380e2
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-10-04 11:33:47 +0200

    Added twitter service task to process for live demo / hacking

---
+++ src/main/java/io/berndruecker/onboarding/customer/process/TwitterAdapter.java
@@ -0,0 +1,57 @@
+package io.berndruecker.onboarding.customer.process;
+
+import io.camunda.zeebe.spring.client.annotation.ZeebeVariablesAsType;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+@Component
+public class TwitterAdapter {
+
+    @Value("${twitterConsumerKey}")
+    private String consumerKey;
+    @Value("${twitterConsumerSecret}")
+    private String consumerSecret;
+    @Value("${twitterAccessToken}")
+    private String accessToken;
+    @Value("${twitterAccessTokenSecret}")
+    private String accessTokenSecret;
+
+    @ZeebeWorker(type="twitter")
+    public Status publishTweet(@ZeebeVariablesAsType TwitterInput input) throws TwitterException {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret);
+
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+
+        String content = input.getTweetContent();
+        //tweetInput.tweetContent = input.getTweetContent() + "...";
+
+        System.out.println("Tweeting: " + content);
+
+        Status status = twitter.updateStatus(content);
+
+        return status;
+    }
+}
+
+
+
+
+/*
+        String consumerKey = outboundConnectorContext.getSecretStore().replaceSecret("secrets.twitterConsumerKey");
+        String consumerSecret = outboundConnectorContext.getSecretStore().replaceSecret("secrets.twitterConsumerSecret");
+        String accessToken = outboundConnectorContext.getSecretStore().replaceSecret("secrets.twitterAccessToken");
+        String accessTokenSecret = outboundConnectorContext.getSecretStore().replaceSecret("secrets.twitterAccessTokenSecret");
+
+ */
---
+++ src/main/java/io/berndruecker/onboarding/customer/process/TwitterInput.java
@@ -0,0 +1,15 @@
+package io.berndruecker.onboarding.customer.process;
+
+public class TwitterInput {
+
+    private String tweetContent;
+
+    public String getTweetContent() {
+        return tweetContent;
+    }
+
+    public void setTweetContent(String tweetContent) {
+        this.tweetContent = tweetContent;
+    }
+
+}
