94249c56500af493e7475003b0f43a709bed59fd
Bernd Ruecker <bernd.ruecker@camunda.com>
2023-05-22 15:06:56 +0200

    Adjusted to latest Camunda/Spring Boot version

--- src/main/java/io/berndruecker/onboarding/customer/CustomerOnboardingSpringbootApplication.java
+++ src/main/java/io/berndruecker/onboarding/customer/CustomerOnboardingSpringbootApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
 @Deployment(resources = "classpath:customer-onboarding.bpmn")
 public class CustomerOnboardingSpringbootApplication {
 
